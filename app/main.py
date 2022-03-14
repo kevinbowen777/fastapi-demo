e1fd6d705b659d2fb53f59d4b4d9d03e17585a55
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-13 20:34:23 -0700

    Define basic GET endpoint

--- app/main.py
+++ app/main.py
@@ -4,3 +4,11 @@ from fastapi import FastAPI, APIRouter
 app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
+
+
+@api_router.get("/", status_code=200)
+def root() -> dict:
+    """
+    Root GET
+    """
+    return {"msg": "Hello, World!"}
