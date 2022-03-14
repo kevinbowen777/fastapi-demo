624196cb6861a1379ceb1db7421f55d82d412d79
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-13 20:38:54 -0700

    Apply conditional for calling module directly

--- app/main.py
+++ app/main.py
@@ -15,3 +15,10 @@ def root() -> dict:
 
 
 app.include_router(api_router)
+
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
