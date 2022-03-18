01073ae994c7e16ee11784b0f9f950159fd65fc8
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-17 19:30:56 -0700

    Time path endpoint path operations

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 
 from fastapi import FastAPI, APIRouter, Request, Depends
@@ -29,6 +30,15 @@ def root(
         "index.html",
         {"request": request, "recipes": recipes},
     )
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
