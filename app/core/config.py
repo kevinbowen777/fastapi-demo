5cb2129cf367d8e30dc12a7538bf2b32c09ad168
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-18 19:53:28 -0700

    Add JWT & SUPERUSER bogus vars

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,12 @@ from typing import List, Optional, Union
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
+    JWT_SECRET: str = "TEST_SECRET_DO_NOT_USE_IN_PROD"
+    ALGORITHM: str = "HS256"
+
+    # 60 minutes * 24 hours * 8 days = 8 days
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
+
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
@@ -19,6 +25,7 @@ class Settings(BaseSettings):
 
     SQLALCHEMY_DATABASE_URI: Optional[str] = "sqlite:///recipes.db"
     FIRST_SUPERUSER: EmailStr = "kevin.bowen@gmail.com"
+    FIRST_SUPERUSER_PW: str = "CHANGEME"
 
     class Config:
         case_sensitive = True
