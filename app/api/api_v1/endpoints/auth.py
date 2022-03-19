e095090750540518653cb0c0f9aaca33cf5a9bdd
Kevin Bowen <kevin.bowen@gmail.com>
2022-03-18 17:13:01 -0700

    Define User signup auth endpoint

---
+++ app/api/api_v1/endpoints/auth.py
@@ -0,0 +1,37 @@
+from typing import Any
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm.session import Session
+
+from app import crud
+from app import schemas
+from app.api import deps
+from app.core.auth import (
+    authenticate,
+    create_access_token,
+)
+from app.models.user import User
+
+router = APIRouter()
+
+
+@router.post("/signup", response_model=schemas.User, status_code=201)
+def create_user_signup(
+    *,
+    db: Session = Depends(deps.get_db),
+    user_in: schemas.user.UserCreate,
+) -> Any:
+    """
+    Create new user without the need to be logged in.
+    """
+
+    user = db.query(User).filter(User.email == user_in.email).first()
+    if user:
+        raise HTTPException(
+            status_code=400,
+            detail="The user with this email already exists in the system",
+        )
+    user = crud.user.create(db=db, obj_in=user_in)
+
+    return user
